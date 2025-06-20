1561c7616e57e9b8c0e763641a922e295dca45dc
Guilherme Sabino Queiroz <guilherme.sabino@hinovamobile.com.br>
2025-06-20 17:26:08 -0300

    Initial Commit

---
+++ Cara ou Coroa/Cara_ou_CoroaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cara_ou_CoroaApp.swift
+//  Cara ou Coroa
+//
+//  Created by Guilherme Sabino Queiroz on 20/06/25.
+//
+
+import SwiftUI
+
+@main
+struct Cara_ou_CoroaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cara ou Coroa/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cara ou Coroa
+//
+//  Created by Guilherme Sabino Queiroz on 20/06/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
